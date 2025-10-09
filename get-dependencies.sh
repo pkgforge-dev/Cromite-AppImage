a5d77d614266451f4ff3882334ff3e4a0728c92a
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-10-08 21:09:06 -0400

    add `ffmpeg-mini`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -35,4 +35,4 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-common --prefer-nano intel-media-driver-mini
+./get-debloated-pkgs.sh --add-common --prefer-nano intel-media-driver-mini ffmpeg-mini
