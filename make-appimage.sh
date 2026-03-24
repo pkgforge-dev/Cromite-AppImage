1a3c453c1f8e1e5fce5a355794a741036eb5ef5f
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-03-24 17:37:33 -0400

    simplify deployment

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,9 +11,7 @@ export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 export DEPLOY_PIPEWIRE=1
 export DEPLOY_QT=1
-export DEPLOY_GTK=1
 export DEPLOY_P11KIT=1
-export GTK_DIR=gtk-3.0
 export URUNTIME_PRELOAD=1 # really needed here
 
 # strip cromite bundled libs
@@ -21,14 +19,15 @@ strip -s -R .comment --strip-unneeded ./AppDir/bin/lib*.so*
 
 # Deploy dependencies
 quick-sharun ./AppDir/bin/chrome -- google.com --no-sandbox
-quick-sharun \
-	./AppDir/bin/chrome_*         \
+STRACE_MODE=0 quick-sharun \
+	./AppDir/bin/chrome*          \
 	./AppDir/bin/libqt6_shim.so*  \
 	/usr/lib/libQt6Widgets.so*    \
 	/usr/lib/libnss*              \
 	/usr/lib/libsoftokn3.so       \
 	/usr/lib/libfreeblpriv3.so    \
-	/usr/lib/libcloudproviders*
+	/usr/lib/libcloudproviders*   \
+	/usr/lib/libgtk-3.so*
 
 # Weird
 ln -s ../bin/chrome ./AppDir/shared/bin/exe
